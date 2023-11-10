d934d113f2ec5168df34e7b71ceddf08e879084a
jeisaraja <jeisa.raja.jrt@gmail.com>
2023-11-10 20:43:03 +0700

    add atlas db connection

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
 import express  from "express";
+import mongoose from "mongoose";
+import 'dotenv/config'
 import { router as usersRouter } from "./routes/users";
+
 const app = express()
+mongoose.connect(process.env.MONGODB_URI! )
+.then(()=>console.log('DB connection success'))
+.catch(error => console.log(error));
+
 app.get('/', (req,res)=>{
   res.send('Hello world!')
 })
